d72ec6c98e431c430295e9613799065fe1765c1a
JustSama <106009367+JustSama@users.noreply.github.com>
2024-10-16 23:55:57 +0500

    Add files via upload

---
+++ HomeWork/Observer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IObserver
+{
+    void Update(decimal newRate);
+}
+
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify();
+}
+
+public class CurrencyExchange : ISubject
+{
+    private List<IObserver> _observers = new List<IObserver>();
+    private decimal _rate;
+
+    public void Attach(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(_rate);
+        }
+    }
+
+    public void SetRate(decimal rate)
+    {
+        _rate = rate;
+        Notify();
+    }
+}
+
+public class Investor : IObserver
+{
+    public void Update(decimal newRate)
+    {
+        Console.WriteLine($"Инвестор: новый курс валюты {newRate}");
+    }
+}
+
+public class Bank : IObserver
+{
+    public void Update(decimal newRate)
+    {
+        Console.WriteLine($"Банк: обновление курса валюты {newRate}");
+    }
+}
+
+public class NewsAgency : IObserver
+{
+    public void Update(decimal newRate)
+    {
+        Console.WriteLine($"Новостное агентство: курс валюты изменился на {newRate}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CurrencyExchange currencyExchange = new CurrencyExchange();
+
+        Investor investor = new Investor();
+        Bank bank = new Bank();
+        NewsAgency newsAgency = new NewsAgency();
+
+        currencyExchange.Attach(investor);
+        currencyExchange.Attach(bank);
+        currencyExchange.Attach(newsAgency);
+
+        currencyExchange.SetRate(1.23m);
+        currencyExchange.SetRate(1.45m);
+
+        currencyExchange.Detach(bank);
+        currencyExchange.SetRate(1.67m);
+    }
+}
---
+++ HomeWork/Strateg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public interface IPaymentStrategy
+{
+    void Pay(decimal amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата {amount} с использованием кредитной карты.");
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата {amount} через PayPal.");
+    }
+}
+
+public class CryptoPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата {amount} с использованием криптовалюты.");
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy _paymentStrategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void ExecutePayment(decimal amount)
+    {
+        _paymentStrategy.Pay(amount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PaymentContext paymentContext = new PaymentContext();
+
+        paymentContext.SetPaymentStrategy(new CreditCardPayment());
+        paymentContext.ExecutePayment(100);
+
+        paymentContext.SetPaymentStrategy(new PayPalPayment());
+        paymentContext.ExecutePayment(200);
+
+        paymentContext.SetPaymentStrategy(new CryptoPayment());
+        paymentContext.ExecutePayment(300);
+    }
+}
